4886b35d582570dada180b7549ec9e89b8ee3de0
Давиденко Александр Арсеньевич <davaa@plant.elesy.com>
2024-07-05 11:21:24 +0700

    send.php

--- send.php
+++ send.php
@@ -1,34 +1,95 @@
 <?php
 
-$token = "7238360214:AAED9R27wBHgJVnU4nCw4LavUxrZ0L9gubA";
+// Токен
+  const TOKEN = '7238360214:AAED9R27wBHgJVnU4nCw4LavUxrZ0L9gubA';
 
-$chat_id = "170195649";
+  // ID чата
+  const CHATID = '170195649';
 
-if ($_POST['act'] == 'order') {
-    $name = ($_POST['id']);
-    $phone = ($_POST['name']);
-    $email = ($_POST['price']);
-    $textarea = ($_POST['order']);
+  // Массив допустимых значений типа файла.
+  $types = array('image/gif', 'image/png', 'image/jpeg', 'application/pdf');
 
-    $arr = array(
-        'Имя:' => $name,
-        'Телефон:' => $phone,
-        'Почта' => $email,
-        'Текст' => $textarea
-    );
-    foreach($arr as $key => $value) {
-        $txt .= "<b>".$key."</b> ".$value."%0A";
-    };
+  // Максимальный размер файла в килобайтах
+  // 1048576; // 1 МБ
+  $size = 1073741824; // 1 ГБ
 
-    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    if ($sendToTelegram) {
-        alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
+  $fileSendStatus = '';
+  $textSendStatus = '';
+  $msgs = [];
+
+  // Проверяем не пусты ли поля с именем и телефоном
+  if (!empty($_POST['name']) && !empty($_POST['price']) && !empty($_POST['order'])) {
+
+    // Если не пустые, то валидируем эти поля и сохраняем и добавляем в тело сообщения. Минимально для теста так:
+    $txt = "";
+
+    // Имя
+    if (isset($_POST['name']) && !empty($_POST['name'])) {
+        $txt .= "Имя пославшего: " . strip_tags(trim(urlencode($_POST['name']))) . "%0A";
     }
 
-    else {
-        alert('Что-то пошло не так. ПОпробуйте отправить форму ещё раз.');
+    // Номер телефона
+    if (isset($_POST['price']) && !empty($_POST['price'])) {
+        $txt .= "Телефон: " . strip_tags(trim(urlencode($_POST['price']))) . "%0A";
     }
+
+    // Не забываем про тему сообщения
+    if (isset($_POST['order']) && !empty($_POST['order'])) {
+        $txt .= "Тема: " . strip_tags(urlencode($_POST['order']));
+    }
+
+    $textSendStatus = @file_get_contents('https://api.telegram.org/bot'. TOKEN .'/sendMessage?chat_id=' . CHATID . '&parse_mode=html&text=' . $txt);
+
+    if( isset(json_decode($textSendStatus)->{'ok'}) && json_decode($textSendStatus)->{'ok'} ) {
+      if (!empty($_FILES['files']['tmp_name'])) {
+
+          $urlFile =  "https://api.telegram.org/bot" . TOKEN . "/sendMediaGroup";
+
+          // Путь загрузки файлов
+          $path = $_SERVER['DOCUMENT_ROOT'] . '/telegramform/tmp/';
+
+          // Загрузка файла и вывод сообщения
+          $mediaData = [];
+          $postContent = [
+            'chat_id' => CHATID,
+          ];
+
+          for ($ct = 0; $ct < count($_FILES['files']['tmp_name']); $ct++) {
+            if ($_FILES['files']['name'][$ct] && @copy($_FILES['files']['tmp_name'][$ct], $path . $_FILES['files']['name'][$ct])) {
+              if ($_FILES['files']['size'][$ct] < $size && in_array($_FILES['files']['type'][$ct], $types)) {
+                $filePath = $path . $_FILES['files']['name'][$ct];
+                $postContent[$_FILES['files']['name'][$ct]] = new CURLFile(realpath($filePath));
+                $mediaData[] = ['type' => 'document', 'media' => 'attach://'. $_FILES['files']['name'][$ct]];
+              }
+            }
+          }
+
+          $postContent['media'] = json_encode($mediaData);
+
+          $curl = curl_init();
+          curl_setopt($curl, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);
+          curl_setopt($curl, CURLOPT_URL, $urlFile);
+          curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+          curl_setopt($curl, CURLOPT_POSTFIELDS, $postContent);
+          $fileSendStatus = curl_exec($curl);
+          curl_close($curl);
+          $files = glob($path.'*');
+          foreach($files as $file){
+            if(is_file($file))
+              unlink($file);
+          }
+      }
+      echo json_encode('SUCCESS');
+    } else {
+      echo json_encode('ERROR');
+      //
+      // echo json_decode($textSendStatus);
+    }
+  } else {
+    echo json_encode('NOTVALID');
+  }
+} else {
+  header("Location: /");
 }
-
-?>
