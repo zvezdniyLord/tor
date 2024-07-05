5b100651bd2a0a97ed63d911b7c70d3e5daa355e
Давиденко Александр Арсеньевич <davaa@plant.elesy.com>
2024-07-05 11:27:23 +0700

    sendd.php

--- send.php
+++ send.php
@@ -6,12 +6,6 @@
   // ID чата
   const CHATID = '170195649';
 
-  // Массив допустимых значений типа файла.
-  $types = array('image/gif', 'image/png', 'image/jpeg', 'application/pdf');
-
-  // Максимальный размер файла в килобайтах
-  // 1048576; // 1 МБ
-  $size = 1073741824; // 1 ГБ
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -41,52 +35,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     $textSendStatus = @file_get_contents('https://api.telegram.org/bot'. TOKEN .'/sendMessage?chat_id=' . CHATID . '&parse_mode=html&text=' . $txt);
-
-    if( isset(json_decode($textSendStatus)->{'ok'}) && json_decode($textSendStatus)->{'ok'} ) {
-      if (!empty($_FILES['files']['tmp_name'])) {
-
-          $urlFile =  "https://api.telegram.org/bot" . TOKEN . "/sendMediaGroup";
-
-          // Путь загрузки файлов
-          $path = $_SERVER['DOCUMENT_ROOT'] . '/telegramform/tmp/';
-
-          // Загрузка файла и вывод сообщения
-          $mediaData = [];
-          $postContent = [
-            'chat_id' => CHATID,
-          ];
-
-          for ($ct = 0; $ct < count($_FILES['files']['tmp_name']); $ct++) {
-            if ($_FILES['files']['name'][$ct] && @copy($_FILES['files']['tmp_name'][$ct], $path . $_FILES['files']['name'][$ct])) {
-              if ($_FILES['files']['size'][$ct] < $size && in_array($_FILES['files']['type'][$ct], $types)) {
-                $filePath = $path . $_FILES['files']['name'][$ct];
-                $postContent[$_FILES['files']['name'][$ct]] = new CURLFile(realpath($filePath));
-                $mediaData[] = ['type' => 'document', 'media' => 'attach://'. $_FILES['files']['name'][$ct]];
-              }
-            }
-          }
-
-          $postContent['media'] = json_encode($mediaData);
-
-          $curl = curl_init();
-          curl_setopt($curl, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);
-          curl_setopt($curl, CURLOPT_URL, $urlFile);
-          curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-          curl_setopt($curl, CURLOPT_POSTFIELDS, $postContent);
-          $fileSendStatus = curl_exec($curl);
-          curl_close($curl);
-          $files = glob($path.'*');
-          foreach($files as $file){
-            if(is_file($file))
-              unlink($file);
-          }
-      }
-      echo json_encode('SUCCESS');
-    } else {
-      echo json_encode('ERROR');
-      //
-      // echo json_decode($textSendStatus);
-    }
   } else {
     echo json_encode('NOTVALID');
   }
